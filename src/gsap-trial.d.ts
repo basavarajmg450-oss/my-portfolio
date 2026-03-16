32bef00d31c91e14d44a916737ad77dd75ee334d
basavaraj mg <basavarajmg450@gmail.com>
2026-03-16 23:02:00 +0530

    Replace gsap-trial with standard gsap package for ScrollSmoother and SplitText

--- src/gsap-trial.d.ts
+++ src/gsap-trial.d.ts
@@ -1,9 +1,9 @@
-declare module 'gsap-trial/ScrollSmoother' {
+declare module 'gsap/ScrollSmoother' {
   export const ScrollSmoother: any;
   export type ScrollSmoother = any;
 }
 
-declare module 'gsap-trial/SplitText' {
+declare module 'gsap/SplitText' {
   export const SplitText: any;
   export type SplitText = any;
 }
