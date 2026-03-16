cd03f1f93fb2870bf94a40ea5db17adc97839f99
basavaraj mg <basavarajmg450@gmail.com>
2026-03-16 22:49:58 +0530

    Fix TypeScript errors for gsap-trial modules to resolve Vercel build failure

---
+++ src/gsap-trial.d.ts
@@ -0,0 +1,9 @@
+declare module 'gsap-trial/ScrollSmoother' {
+  export const ScrollSmoother: any;
+  export type ScrollSmoother = any;
+}
+
+declare module 'gsap-trial/SplitText' {
+  export const SplitText: any;
+  export type SplitText = any;
+}
